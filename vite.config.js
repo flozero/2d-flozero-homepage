56dbd18e4043c350f7e475912d67c512a1fc001e
flozero <hello@florent.dev>
2025-02-24 02:03:25 -0500

    chore: fix vite cofnig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/2d-flozero-homepage",
+  base: "/2d-flozero-homepage/",
   plugins: [react()],
 });
